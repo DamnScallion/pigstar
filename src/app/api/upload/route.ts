fc6eab7d8d070220a734e44456e017f6e307920c
George Luo <focusdlaw@gmail.com>
2025-03-22 03:14:47 +1100

    style: ModeToggle button

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,32 +1,3 @@
-// import { v2 as cloudinary } from 'cloudinary';
-// import { NextResponse } from 'next/server';
-// import * as dotenv from "dotenv";
-
-// dotenv.config();
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// export async function POST(request: Request) {
-//   const { file } = await request.json();
-  
-//   try {
-//     const result = await cloudinary.uploader.upload(file, {
-//       upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
-//     });
-//     return NextResponse.json({ url: result.secure_url });
-//   } catch (error) {
-//     return NextResponse.json(
-//       { error: 'Upload failed' },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-
 import { v2 as cloudinary } from "cloudinary";
 import { NextResponse } from "next/server";
 import * as dotenv from "dotenv";
